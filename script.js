5bc18d6a0d356e2ede2613b44e9bcd3faf024b86
philpatt <phillip.s.patterson1@gmail.com>
2018-01-25 09:06:05 -0800

    commit

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ $(document).ready(function() {
 
   $(window).scroll(function() {
     console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 200) {
+    if ($(window).scrollTop() > 400) {
       $('.navbar').addClass('navbar-fixed-top fade-in');
       $('.navbar').removeClass('hidden fade-out');
 
     }
-    if ($(window).scrollTop() < 701) {
+    if ($(window).scrollTop() < 401) {
       $('.navbar').removeClass('fade-in');
       $('.navbar').addClass('fade-out');
     }
   });
+
+  $(".nav-link").click(function() {
+      $('html,body').animate({
+          scrollTop: $("#home #about #work #contact").offset().top},
+          'slow');
+  });
 });
+
