31d4fa4b8d638678ec065de5f20ecf8d62c413af
philpatt <phillip.s.patterson1@gmail.com>
2018-02-13 22:23:37 -0800

    responsive work

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
+
   $(window).scroll(function() {
     console.log($(window).scrollTop())
     if ($(window).scrollTop() > 400) {
       $('.navbar').addClass('navbar-fixed-top fade-in');
       $('.navbar').removeClass('hidden fade-out');
-
     }
     if ($(window).scrollTop() < 401) {
       $('.navbar').removeClass('fade-in');
@@ -14,22 +14,25 @@ $(document).ready(function() {
   });
 });
 
+
+
 function mouseEnter(pNum,skillA,skillB,skillC,skillD) {
   console.log(pNum,skillA,skillB,skillC,skillD);
   var projImg = document.getElementById('image');
-  var skill = document.getElementById('skill');
   var proj = document.getElementById('p');
+  var idle = document.getElementById('idle-image');  
   console.log(skillA);
   projImg.src ="img/projects/proj"+pNum+".jpg";
   proj.style.visibility = 'visible';
-  skillA.style.backgroundColor = 'yellow';
 }
 
-function mouseLeave(pNum) {
+function mouseLeave(pNum, skillA, skillB, skillC, skillD) {
   var projImg = document.getElementById('image');
   var proj = document.getElementById('p');
+  var idle = document.getElementById('idle-image');
   projImg.src = "";
-  proj.style.visibility = 'hidden';  
+  proj.style.visibility = 'hidden';
+  
   // projImg.src = "img/projects/proj" + pNum + ".png";
 
 }
