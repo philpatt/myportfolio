5bd4d17a5e36526cff5ac57708c8ef539ca2e40b
philpatt <phillip.s.patterson1@gmail.com>
2018-02-26 21:31:47 -0800

    scroll feature is done

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
   $(window).scroll(function() {
     console.log($(window).scrollTop())
     if ($(window).scrollTop() > 400) {
@@ -12,9 +10,34 @@ $(document).ready(function() {
       $('.navbar').addClass('fade-out');
     }
   });
+
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          console.log('scrollin');
+        });
+      }
+    }
+  });
 });
-
-
-
 
 
